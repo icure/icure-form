8f25963ceaf276f73b50e52b3f5d53f9cb5e773b
aduchate <ad@icure.com>
2024-01-01 19:57:47 +0100

    Better formulas

--- app/form-values-container.ts
+++ app/form-values-container.ts
@@ -35,18 +35,62 @@ export const makeFormValuesContainer = () => {
 		],
 	})
 
-	const entity = {
-		getEntityReference: () => {
-			return Promise.resolve('entity')
-		},
+	const sb = new Map()
+	const cs: Map<string, any> = new Map()
+	const interpreter = <T, S extends { [key: string | symbol]: unknown }>(
+		formula: string,
+		getVersions: (filter: (s: Service) => boolean) => { [key: string]: Service },
+		sandbox: S = new Proxy({} as S, {
+			has: (target: S, key: string | symbol) => Object.keys(this.getVersions((s) => s.label === key) ?? {}).length > 0,
+			get: (target: S, key: string | symbol) => Object.values(this.getVersions((s) => s.label === key)),
+		}),
+	): T | undefined => {
+		function compileCode(src: string) {
+			if (cs.has(src)) {
+				return cs.get(src)
+			}
+
+			src = 'with (sandbox) {' + src + '}'
+			const code = new Function('sandbox', src)
+
+			const result = function (sandbox: S) {
+				if (!sb.has(sandbox)) {
+					const sandboxProxy = new Proxy<S>(sandbox, { has, get })
+					sb.set(sandbox, sandboxProxy)
+				}
+				return code(sb.get(sandbox))
+			}
+
+			cs.set(src, result)
+
+			return result
+		}
+
+		function has() {
+			return true
+		}
+
+		function get(target: S, key: string | symbol) {
+			if (key === Symbol.unscopables) return undefined
+			return target[key]
+		}
+
+		let compiledCode: any
+		try {
+			compiledCode = compileCode(formula)
+		} catch (e) {
+			console.info('Invalid Formula: ' + formula)
+			return undefined
+		}
+		try {
+			const result = compiledCode(sandbox)
+			return result
+		} catch (e) {
+			console.info('Error while executing formula: ' + formula, e)
+			return undefined
+		}
 	}
-	const interpretor: (formula: string, sandbox: any) => Promise<any> = async (formula, sandbox) => {
-		const sandboxKeys = [...Object.keys(sandbox), 'entity']
-		sandbox['entity'] = entity
-		const sandboxValues = sandboxKeys.map((k) => sandbox[k])
-		const f = new Function(...sandboxKeys, `return ${formula}`)
-		return await f(...sandboxValues)
-	}
+
 	const now = +new Date()
-	return new ContactFormValuesContainer(cc, [ctc], (label, serviceId) => new Service({ label, id: serviceId ?? uuid(), created: now, modified: now }), interpretor)
+	return new ContactFormValuesContainer(cc, [ctc], (label, serviceId) => new Service({ label, id: serviceId ?? uuid(), created: now, modified: now }), interpreter)
 }
